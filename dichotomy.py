366bf83ff17537b1ab2f0434c45e4cf8be8f5b14
Dill <60818081+Di-Liks@users.noreply.github.com>
2024-03-31 16:41:24 +0300

    Create dichotomy.py

---
+++ dichotomy.py
@@ -0,0 +1,22 @@
+def f(x):
+    return 2*x**2 - 2*x + 5/2
+def dichotomy_method(a, b, epsilon):
+    iterations = 0
+    while (b - a) / 2 > epsilon:
+        midpoint = (a + b) / 2
+        if f(midpoint) == 0:
+            return midpoint
+        elif f(a) * f(midpoint) < 0:
+            b = midpoint
+        else:
+            a = midpoint
+        iterations += 1
+        print(f"Итерация {iterations}: a = {a}, b = {b}, ср. значение = {midpoint}")
+    return (a + b) / 2
+
+a = 1
+b = 9
+epsilon = 0.2
+
+root = dichotomy_method(a, b, epsilon)
+print("Точка:", root)
