788628f31eb154f3c25459afd6915ef35abe207d
Dill <60818081+Di-Liks@users.noreply.github.com>
2024-03-31 17:03:25 +0300

    Update dichotomy.py

--- dichotomy.py
+++ dichotomy.py
@@ -1,23 +1,33 @@
 def f(x):
-    return 2*x**2 - 2*x + 5/2
+    return 2 * x**2 - 2 * x + 5/2
+
 def dichotomy_method(a, b, epsilon):
     iterations = 0
-    while (b - a) / 2 > epsilon:
-        midpoint = (a + b) / 2
-        if f(midpoint) == 0:
-            return midpoint
-        elif f(a) * f(midpoint) < 0:
-            b = midpoint
-        else:
-            a = midpoint
+    while (b - a) >= epsilon:
         iterations += 1
-        print(f"Итерация {iterations}: a = {a}, b = {b}, ср. значение = {midpoint}")
+        # Находим середину отрезка
+        c = (a + b) / 2
+        # Вычисляем две точки с отступом epsilon от середины
+        c1 = c - epsilon
+        c2 = c + epsilon
+        # Сравниваем значения функции в точках c1 и c2
+        if f(c1) < f(c2):
+            b = c
+        else:
+            a = c
+        print(f"Итерация {iterations}: [a, b] = [{a}, {b}], точка минимума = {(a + b) / 2}")
+
+    # Возвращаем середину полученного отрезка
     return (a + b) / 2
 
+# Задаем начальные значения
 a = 1
 b = 9
 epsilon = 0.2
 
-root = dichotomy_method(a, b, epsilon)
-print("Точка:", root)
-print("Значение функции в точке:", f(root))
+# Вызываем метод дихотомии
+min_x = dichotomy_method(a, b, epsilon)
+min_value = f(min_x)
+
+print("\nТочка минимума:", min_x)
+print("Значение в точке:", min_value)
